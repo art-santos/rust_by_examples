912cc3b5e639eb745ac2803a6f080bf16541d557
art-santos <arthursantosdeveloper@gmail.com>
2022-05-15 15:15:32 -0300

    chore: structures

---
+++ 2_day/6_structures/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    family_struct()
+}
+
+/*
+ * Structures work pretty much like interfaces in JS.
+ * They can be created to make a more
+ * visible sense in your code in general terms.
+*/
+
+fn family_struct() {
+    struct Family {
+        dad: String,
+        mom: String,
+    }
+
+    let family_morisson = Family {
+        dad: "John Morrison".to_string(),
+        mom: "Jane Morrison".to_string(),
+    };
+
+    println!("Dad's Name: {} - Mom's name: {}", family_morisson.dad, family_morisson.mom);
+}

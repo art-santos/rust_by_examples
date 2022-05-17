34a154e63952eee4e824fe3db5bc87d91c78c66f
art-santos <arthursantosdeveloper@gmail.com>
2022-05-17 16:18:35 -0300

    chore: irrefutable pattern

--- 03_day/16_irrefutable_patterns/src/main.rs
+++ 03_day/16_irrefutable_patterns/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    irrefutable_family();
+}
+/*
+ * Is this pattern about Jordan Peterson? I don't think so.
+ * An irrefutable pattern when there's only one and always correct way
+ * to match this. Sounds confusing? Let's see an example. 
+ */
+
+
+fn irrefutable_family(){
+    let family = (String::from("Dad"), String::from("Mom"));
+    let (dad, mom) = family;
+
+    println!("{} {}", dad, mom);
 }

e8dad24a8cafeeb6400d59b677bf5c55315a61df
art-santos <arthursantosdeveloper@gmail.com>
2022-05-18 20:05:55 -0300

    chore: for loops

--- 05_day/25_for_loops/src/main.rs
+++ 05_day/25_for_loops/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+   family_loop();
+}
+/*
+ * For loops are very common in basically every programming language
+ * a for..loop consists of three parts:
+ * a condition that need to be meet in order to continue the loop
+ * or reached in order to stop the loop.
+ * ??A data structure that will be iterated over (but not necessarily).
+ * A return for the data structure iteration
+ */
+
+fn family_loop(){
+    let mut array = [1, 2, 3, 4, 5];
+    for element in array.iter_mut() {
+        *element += 1;
+    }
+    println!("{:?}", array);
+
+    //We can also use this to print all items we want
+
+    for x in 0..1000001 {
+        if x == 1000000 {
+                println!("{}", x);
+        }
+    }
 }

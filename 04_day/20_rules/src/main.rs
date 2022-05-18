1db645844c5b612d3584de47efbabc8a1778198b
art-santos <arthursantosdeveloper@gmail.com>
2022-05-18 13:50:15 -0300

    chore: rules

--- 04_day/20_rules/src/main.rs
+++ 04_day/20_rules/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/*
+ *Some rules must be explained befdore we go foward:
+ *If a trait doesn't exists in the scope of the function,
+ *it then must be imported.
+ *
+ * The implementation of the trait must be in the same 
+ * crate as the trait or type.  
+ */

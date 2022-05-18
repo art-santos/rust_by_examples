8c41de583d2de87a0bd3f6bff3351ef895b209d6
art-santos <arthursantosdeveloper@gmail.com>
2022-05-18 13:46:00 -0300

    chore: associated types (incomplete)

--- 04_day/19_associated_types/src/main.rs
+++ 04_day/19_associated_types/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    trait Graph {
+        type N;
+        type E;
+    
+        fn has_edge(&self, &Self::N, &Self::N) -> bool;
+        fn edges(&self, &Self::N) -> Vec<Self::E>;
+        
+        fn distance<G: Graph>(graph: &G, start: &G::N, end: &G::N) -> u32 { ... }
+
+    }
 }

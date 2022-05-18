27ac59e3d9881ab54e4ed5a68f456222540cf362
art-santos <arthursantosdeveloper@gmail.com>
2022-05-18 17:35:47 -0300

    chore: optional types

--- 04_day/22_option_type/src/main.rs
+++ 04_day/22_option_type/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/**
+ * Optional types are vey useful in the case of the absence of 
+ * a value.
+ */
+
+
+fn family_option_type(){
+    enum Option<T>{
+        Some(T),
+    }
+    let x = Option::Some(1);
+    let y = Option::Some('a');
+}

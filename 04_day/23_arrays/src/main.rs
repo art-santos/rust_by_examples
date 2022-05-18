5a0a4a018f8ed5e5576316c88bc4ceb684c02c60
art-santos <arthursantosdeveloper@gmail.com>
2022-05-18 18:04:05 -0300

    chore: arrays

--- 04_day/23_arrays/src/main.rs
+++ 04_day/23_arrays/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    family_array();
+}
+
+
+/*
+ * Arrays are a collection of elements.
+ * Different from tuples, arrays can contain different types.
+ * And they do not have a fixed amount of values.
+ */
+
+fn family_array(){
+    let arr1 = [1, 2, 3, 4, 5];
+    let arr2: [u8; 5] = [1, 2, 3, 4, 5];
+
+    println!("{}", arr1[0]);
+    println!("{}", arr2[0]);
 }

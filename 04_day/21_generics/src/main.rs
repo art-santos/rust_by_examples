324d5c1f91baf2dc54b4dc562568710a3cd279d2
art-santos <arthursantosdeveloper@gmail.com>
2022-05-18 17:25:20 -0300

    chore: generics

--- 04_day/21_generics/src/main.rs
+++ 04_day/21_generics/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", family_generics(1, 0).0);
+}
+
+
+/*
+ *Generics working by making a function more open to new
+ *types. So, this way, we can avoid code duplication. 
+ */
+
+
+/**
+ * What is happening?
+ * After the function declaration, we do use the PartialOrd
+ * trait to make the function more open to new types.
+ * When we associate two different traits, that's called a trait bound
+ * Now, we can associate this function with not just integers,
+ * but floats, u8, i32, etc... The only condition is that both
+ * types shall be equal. 
+ */
+
+
+fn family_generics<T: PartialOrd>(a: T, b: T) -> (T, T) {
+    if a > b {
+        (b, a)
+    } else {
+        (a, b)
+    }
 }

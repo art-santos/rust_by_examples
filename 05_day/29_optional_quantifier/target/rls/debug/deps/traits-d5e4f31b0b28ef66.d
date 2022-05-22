f9218456531eb4f067d91d436da507e462463b89
art-santos <arthursantosdeveloper@gmail.com>
2022-05-21 23:10:28 -0300

    chore: opitional quantifier

--- 05_day/29_optional_quantifier/target/rls/debug/deps/traits-d5e4f31b0b28ef66.d
+++
@@ -1,5 +0,0 @@
-/home/arthur/Programming /Rust/05_day/29_optional_quantifier/target/rls/debug/deps/traits-d5e4f31b0b28ef66.rmeta: src/main.rs
-
-/home/arthur/Programming /Rust/05_day/29_optional_quantifier/target/rls/debug/deps/traits-d5e4f31b0b28ef66.d: src/main.rs
-
-src/main.rs:

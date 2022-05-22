54d7bc9bea32f3f0d8ec49bef80ca49b448d1b1f
art-santos <arthursantosdeveloper@gmail.com>
2022-05-21 23:10:14 -0300

    chore: opitional quantifier

--- 05_day/29_optional_quantifier/src/main.rs
+++ 05_day/29_optional_quantifier/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/*
+ * In the macro rules system, there's no way to specify that
+ * a pattern is opitional.
+ * 
+ * So, for specifing an optoinal quantifier in macros, we must use a , for this.
+ *   ($($key:expr => $value:expr ),* $(,)* ).
+ */
---
+++ 05_day/29_optional_quantifier/target/rls/debug/deps/traits-6d68bf92e30b8ec7.d
@@ -0,0 +1,5 @@
+/home/arthur/Programming /Rust/05_day/29_optional_quantifier/target/rls/debug/deps/traits-6d68bf92e30b8ec7.rmeta: src/main.rs
+
+/home/arthur/Programming /Rust/05_day/29_optional_quantifier/target/rls/debug/deps/traits-6d68bf92e30b8ec7.d: src/main.rs
+
+src/main.rs:
